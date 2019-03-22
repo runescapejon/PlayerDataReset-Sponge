7aa94257fbf46a765521f55d3a31e9a7924e248f
Brycey92 <bbrowner@optonline.net>
2019-03-22 01:03:47 -0400

    Reimplement integrity checks, refactor variables, readd messages, add player name to more messages, change color codes to differentiate between successes and errors, add comments, change default config to backup playerdata

--- src/main/java/me/runescapejon/reset/ConfigSettings.java
+++ src/main/java/me/runescapejon/reset/ConfigSettings.java
@@ -6,14 +6,20 @@ import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 @ConfigSerializable
 public class ConfigSettings {
 	@Setting(value = "OfflineMessage")
-	public static String OfflineMessage = "&cThe player must be offline to reset their playerdata.";
+	public static String OfflineMessage = "&c%player% must be offline to reset their playerdata.";
 
 	@Setting(value = "BackUpPlayerdata", comment = "If true, backup playerdata before resetting it.")
-	public static boolean BackUpPlayerdata = false;
+	public static boolean BackUpPlayerdata = true;
 
 	@Setting(value = "ResetMessage")
-	public static String ResetMessage = "&cReset %player%'s data.";
+	public static String ResetMessage = "&aReset %player%'s data.";
+
+	@Setting(value = "ResetFailedMessage")
+    public static String ResetFailedMessage = "&cError resetting %player%'s data.";
 
 	@Setting(value = "BackupMessage")
-	public static String BackupMessage = "&cA backup has been saved to %backupPath%.";
+	public static String BackupMessage = "&aA backup has been saved to %backupPath%.";
+
+	@Setting(value = "BackupFailedMessage")
+    public static String BackupFailedMessage = "&cError backing up %player%'s data before reset. The reset has been aborted.";
 }
